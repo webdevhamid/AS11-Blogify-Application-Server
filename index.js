6d7535aa5eef989119cef5a8b66f79cbff2d2e7b
webdevhamid <abdulhamid1110000@gmail.com>
2025-10-23 12:22:54 +0600

    Added featured and recent blogs API endpoint

--- index.js
+++ index.js
@@ -1,10 +1,58 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const cors = require("cors");
+const cookieParse = require("cookie-parser");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // Middlewares
 app.use(cors());
+app.use(cookieParse());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w1xw1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const database = client.db("NewsWavesDB");
+    const blogsCollection = database.collection("blogs");
+
+    app.get("/blogs", async (req, res) => {
+      const result = await blogsCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/featured-blogs", async (req, res) => {
+      const result = await blogsCollection.find({ featuredBanner: true }).limit(5).toArray();
+      res.send(result);
+    });
+
+    app.get("/recent-blogs", async (req, res) => {
+      const result = await blogsCollection.find().limit(6).toArray();
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello from the server!");
